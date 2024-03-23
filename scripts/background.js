5025e509443bb5bab0d10a286414aed1a6bddf6e
Amy Cao <caoamyc@sfu.ca>
2024-03-23 16:52:10 -0700

    background, text, font implemented

--- scripts/background.js
+++ scripts/background.js
@@ -1,25 +1,44 @@
+
+// TODO: preserver state of extension(ON/OFF)
 // extension is initally turned off when installed
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.action.setBadgeText({
-      text: 'OFF'
-    });
-});
+// chrome.runtime.onInstalled.addListener(() => {
+//     chrome.action.setBadgeText({
+//       text: 'OFF'
+//     });
+// });
 
-chrome.action.onClicked.addListener(async (tab) => {
 
-    // We retrieve the action badge to check if the extension is 'ON' or 'OFF'
-    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-    console.log("tab.id:", tab.id)
-    // Next state will always be the opposite
-    const nextState = prevState === 'ON' ? 'OFF' : 'ON';
+// // // When the user clicks on the extension action
+// chrome.action.onClicked.addListener(async (tab) => {
+
+//     // We retrieve the action badge to check if the extension is 'ON' or 'OFF'
+//     const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+//     console.log("tab.id:", tab.id)
+//     // Next state will always be the opposite
+//     const nextState = prevState === 'ON' ? 'OFF' : 'ON';
   
-    // Set the action badge to the next state
-    await chrome.action.setBadgeText({
-      tabId: tab.id,
-      text: nextState
-    });
-});
-
+//     // Set the action badge to the next state
+//     await chrome.action.setBadgeText({
+//       tabId: tab.id,
+//       text: nextState
+//     });
+  
+//     if (nextState === 'ON') {
+//       // Insert the CSS file when the user turns the extension on
+//       await chrome.scripting.insertCSS({
+//         files: ['css/colors.css'],
+//         target: { tabId: tab.id }
+//       });
+//       console.log("inside if statement tab.id:", tab.id)
+//     } else if (nextState === 'OFF') {
+//       // Remove the CSS file when the user turns the extension off
+//       await chrome.scripting.removeCSS({
+//         files: ['css/colors.css'],
+//         target: { tabId: tab.id }
+//       });
+//     }
+    
+//   });
     
 chrome.tabs.query({}, (tabs) => tabs.forEach( tab => 
     chrome.scripting.insertCSS({
@@ -28,14 +47,6 @@ chrome.tabs.query({}, (tabs) => tabs.forEach( tab =>
 })));
 
 
-// chrome.tabs.onCreated.addListener( async () => {
-//     console.log("new tab opened")
-//     chrome.tabs.query({}, (tabs) => tabs.forEach( tab => 
-//     chrome.scripting.insertCSS({
-//     files: ['css/colors.css'],
-//     target: { tabId: tab.id }
-// })))
-// })
 function applyColor(){
     chrome.tabs.query({}, (tabs) => tabs.forEach( tab => 
         chrome.scripting.insertCSS({
