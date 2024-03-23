52dfbc4145175388b5a3197e24416f671cfdd3a2
Kelly <kellyk2603@gmail.com>
2024-03-23 13:55:42 -0700

    update background

--- background/background.js
+++ background/background.js
@@ -1,12 +1,13 @@
 function changeBackgroundImage () {
-    document.body.style.backgroundImage = "./assets/images/cat_image.jpg" ;
+    //document.body.style.backgroundImage = 'url("./assets/images/cat_image.jpg")' ;
+    document.body.style.backgroundImage = 'url("https://upload.wikimedia.org/wikipedia/commons/a/a5/Red_Kitten_01.jpg")';
 }
 
 chrome.action.onClicked.addListener((tab) => {
     if(!tab.url.includes('chrome://')) {
         chrome.scripting.executeScript({
             target: {tabId: tab.id},
-            function: changeBackgroundImage
+            func: changeBackgroundImage
         });
     }
 });
