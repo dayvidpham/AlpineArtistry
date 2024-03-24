240ae066835c236f1d049b163fbd0232e909cf31
dayvidpham <dayvidpham@gmail.com>
2024-03-23 18:31:18 -0700

    Add audio playback on extension badge click

--- scripts/background.js
+++ scripts/background.js
@@ -84,3 +84,34 @@ chrome.webNavigation.onCommitted.addListener((details) => {
         });
     }
 });
+
+
+
+/**
+ * Plays audio files from extension service workers
+ * @param {string} source - path of the audio file
+ * @param {number} volume - volume of the playback
+ */
+async function playSound(source = '/assets/default.m4a', volume = 1) {
+    await createOffscreen();
+    await chrome.runtime.sendMessage({ 
+        type: "play_audio",
+        data: { 
+            audio: { source, volume }
+        }
+    });
+}
+
+// Create the offscreen document if it doesn't already exist
+async function createOffscreen() {
+    if (await chrome.offscreen.hasDocument()) return;
+    await chrome.offscreen.createDocument({
+        url: '/offscreen.html',
+        reasons: ['AUDIO_PLAYBACK'],
+        justification: 'testing' // details for using the API
+    });
+}
+
+chrome.action.onClicked.addListener((tab) => {
+    playSound();
+})
