92550afdc4c889d8389a4ec0a579a8c9c87e5c9a
Kelly Feng <16.kelly.feng@gmail.com>
2024-03-23 19:31:15 -0700

    Merge

--- scripts/background.js
+++ scripts/background.js
@@ -74,3 +74,35 @@ chrome.webNavigation.onCommitted.addListener((details) => {
         });
     }
 });
+
+//idle audio
+async function playSound(source = '/sound-effect/anime-wow.mp3', volume = 1) {
+    await createOffscreen();
+    await chrome.runtime.sendMessage({ 
+        type: "play_audio",
+        data: { 
+            audio: { source, volume }
+        }
+    });
+}
+
+// Create the offscreen document if it doesn't already exist
+async function createOffscreen() {
+    if (await chrome.offscreen.hasDocument()) return;
+    await chrome.offscreen.createDocument({
+        url: 'sound-effect/sound.html',
+        reasons: ['AUDIO_PLAYBACK'],
+        justification: 'notif for idling' 
+    });
+}
+
+//15 seconds
+chrome.idle.setDetectionInterval(15);
+
+chrome.idle.onStateChanged.addListener(function(state) {
+    if (state === "idle") {
+    //   setTimeout(function() {
+        playSound();
+    //   }, 10000); //60000 = 1 minute 
+    }
+  });
